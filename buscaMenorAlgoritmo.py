054a2b230a4c20692f3ad14efdbb8ef0f9c93696
JeanSantos00 <jeanlucas00@outlook.com.br>
2025-05-27 19:56:06 -0300

    Estudo do Livro Entendendo Algoritmos - Um Guia Ilustrado Para Programadores e Outros Curiosos - Autor (Aditya Y. Bhargava)

--- buscaMenorAlgoritmo.py
+++ buscaMenorAlgoritmo.py
@@ -1,6 +1,6 @@
 def buscaMenor(arr):
-    menor = arr[0]
-    menor_indice = 0
+    menor = arr[0]  #Armazena o menor valor
+    menor_indice = 0    #Armazena o indice do menor valor
 
     for i in range(1, len(arr)):
         if arr[i] < menor:
@@ -8,8 +8,14 @@ def buscaMenor(arr):
             menor_indice = i
     return menor_indice
 
-def ordenaçãoPorSelecao(arr):
+
+
+def ordenacaoPorSelecao(arr):   #Ordenações em um Array
     novoArr = []
     for i in range(len(arr)):
-        menor = buscaMenor(arr)
+        menor = buscaMenor(arr)     #Encontra o menor elemento do Array e adiciona ao novo Array
         novoArr.append(arr.pop(menor))
+    return novoArr
+
+
+print(ordenacaoPorSelecao([5, 3, 6, 2, 10]))
